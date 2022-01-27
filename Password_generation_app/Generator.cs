19739b2312a00adf14b45b0b3270ce4a34a29a81
Dmitry Siemens <d.siemens71@gmail.com>
2022-01-27 16:44:16 +0200

    create class Password
    create class Password

--- Password_generation_app/Generator.cs
+++ Password_generation_app/Generator.cs
@@ -9,37 +9,59 @@ namespace Password_generation_app
 
     internal class Generator
     {
-        string Password;
-        Random rnd = new Random();
-         string symbols;
+        public string Password { get; set; }
+        Random rnd;
+        private List<string> Numbers;
+        private List<string> Symbols;
+        private List<string> AlphaUpper;
+        private List<string> AlphaLower;
 
+        public Generator()
+        {
+            rnd = new Random(DateTime.Now.Millisecond);
+            Numbers = new List<string>() { "0123456789" };
+            AlphaUpper = new List<string>() { "ABCDEFGHIJKLMNOPQRSTUVWXYZ" };
+            AlphaLower = new List<string>() { "abcdefghijklmnopqrstuvwxyz" };
+            Symbols = new List<string>() { "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ " };
+        }
 
         public string PasswordGenerate(int size, bool num, bool sym, bool UpA, bool LowA)
         {
             Password = null;
             string buf = null;
-
+            
             if (num == true)
             {
-
+                foreach (string s in Numbers)
+                {
+                    buf += s;
+                }
             }
             if (sym == true)
             {
-
+                foreach(string s in Symbols)
+                {
+                    buf += s;
+                }
             }
             if (UpA == true)
             {
-
+                foreach(string s in AlphaUpper)
+                {
+                    buf += s;
+                }
             }
             if (LowA == true)
             {
-
+                foreach (string s in AlphaLower)
+                {
+                    buf += s;
+                }
             }
 
             for (int i = 0; i < size; i++)
             {
                 Password += buf[rnd.Next(buf.Length)];
-
             }
 
 
--- Password_generation_app/MainWindow.xaml.cs
+++ Password_generation_app/MainWindow.xaml.cs
@@ -22,23 +22,79 @@ namespace Password_generation_app
     /// </summary>
     public partial class MainWindow : Window
     {
-       
+        Generator gen = null;
+
         public MainWindow()
         {
             InitializeComponent();
-            
+
+            gen = new Generator();
+
         }
 
 
         private void generButton_Click(object sender, RoutedEventArgs e)
         {
+            int size = int.Parse(textBoxNumber.Text);
+            string symbols = textBoxLeter.Text;
+            bool num = false, sym = false, UpA = false, LowA = false;
 
 
+            if (size < 4 || size > 8)
+            {
+                textBoxNumber.ToolTip = "Это поле введено не корректно";
+                textBoxNumber.Foreground = Brushes.Red;
+            }
+            else
+            {
+                textBoxNumber.ToolTip = "";
+                textBoxNumber.Foreground = Brushes.Black;
+            }
+            if (symbols.Length < 0 || symbols.Length > 50)
+            {
+                textBoxLeter.ToolTip = "Это поле введено не корректно";
+                textBoxLeter.Foreground = Brushes.Red;
+
+            }
+            else
+            {
+                textBoxLeter.ToolTip = "";
+                textBoxLeter.Foreground = Brushes.Black;
+            }
+            if (symbols.Length == 0)
+            {
+                if (checkBoxNumber.IsChecked == true)
+                {
+                    num = true;
+
+                }
+                if (checkBoxSymbol.IsChecked == true)
+                {
+                    sym = true;
+                }
+                if (checkBoxUpperLit.IsChecked == true)
+                {
+                    UpA = true;
+                }
+                if (checkBoxLowerLit.IsChecked == true)
+                {
+                    LowA = true;
+                }
+                if (num == false && sym == false && UpA == false && LowA == false)
+                {
+                    MessageBox.Show("Выберите параметр для генерации пароля");
+                }
+                else
+                {
+                    Password.Text = gen.PasswordGenerate(size, num, sym, UpA, LowA);
+                }
+
+            }
+            else
+            {
+                Password.Text = gen.PasswordGenerate(size, symbols);
+            }
         }
-
-       
-
-        
 
         private void saveButton_Click(object sender, RoutedEventArgs e)
         {
@@ -48,7 +104,7 @@ namespace Password_generation_app
             saveFileDialog.FileName = "MyPassword";
             if (saveFileDialog.ShowDialog() == true)
             {
-                //File.WriteAllText(saveFileDialog.FileName, PasswordBox.Text);
+                File.WriteAllText(saveFileDialog.FileName, Password.Text);
             }
         }
     }
